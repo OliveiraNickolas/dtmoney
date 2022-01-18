63650c9b0e19119b3848085711e24757a6b29b2a
Nickolas Oliveira <nickolasarthur@gmail.com>
2022-01-18 03:01:24 +0000

    Aula 'Criando botões de Tipo' finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+  // Essas funções estão aqui e não dentro do próprio componente do modal, pelo fato de que o botão, que está dentro do Header,
+  // precisa acessar essas informações para executar a sua função de abrir o modal. Então estando tudo aqui, o pai App() passa tudo para
+  // o filho Header que por sua vez consegue passar tudo pro seu filho Button
+
   return (
     <>
-      <Header />  
+      <Header  onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      {/* Essas propriedades estão aqui pois o Button dentro do Header precisa receber as funções para abrir o modal */}
+
+
       <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      // essas são propriedades são da própria configuração do modal pra entender quando abre e quando fecha
+      />
+
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+    // Essa aqui é a propriedade que foi nomeada de "OnOpenNewTransactionModal" 
+    // recebendo a função que o botão (que está aqui dentro do Header) precisa receber lá do App.tsx 
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+    
     return (
         <Container>
             <Content>
-            <img src={logoImg} alt="dt money" />
-            <button type="button">
-                Nova transação
-            </button>
+                <img src={logoImg} alt="dt money" />
+                <button type="button" onClick={onOpenNewTransactionModal}>
+                    Nova transação
+                </button>
             </Content>
         </Container>
     )
