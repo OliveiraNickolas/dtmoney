a5dbffdeccb1ab837a82e1e48a38bcb831706b4b
Nickolas Oliveira <nickolasarthur@gmail.com>
2022-01-20 01:07:54 +0000

    Listado os dados da API

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-01-19 09:15:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2022-01-18 10:15:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
@@ -20,7 +43,7 @@ createServer({
 
       return schema.create('transaction', data)
     })
-  }
+  },
 })
 
 ReactDOM.render(
