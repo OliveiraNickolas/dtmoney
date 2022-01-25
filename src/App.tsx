7ea2da2f771e9ca52dad6292afda11b30ab45631
Nickolas Oliveira <nickolasarthur@gmail.com>
2022-01-25 23:57:36 +0000

    Movendo criação para o context

--- src/App.tsx
+++ src/App.tsx
@@ -19,21 +19,16 @@ export function App() {
   function handleCloseNewTransactionModal() {
       setIsNewTransactionModalOpen(false);
   }
-  // Essas funções estão aqui e não dentro do próprio componente do modal, pelo fato de que o botão, que está dentro do Header,
-  // precisa acessar essas informações para executar a sua função de abrir o modal. Então estando tudo aqui, o pai App() passa tudo para
-  // o filho Header que por sua vez consegue passar tudo pro seu filho Button
-
+  
   return (
     <TransactionsProvider>
       <Header  onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
-      {/* Essas propriedades estão aqui pois o Button dentro do Header precisa receber as funções para abrir o modal */}
-
+  
       <Dashboard />
 
       <NewTransactionModal 
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      // essas são propriedades são da própria configuração do modal pra entender quando abre e quando fecha
       />
 
       <GlobalStyle />
